fde0ed36b1cac67309c297b453006fdb4b41fb14
LukaBean24 <lkochlo24@gmail.com>
2022-03-09 23:25:49 +0400

    submitted applications back arrow added

--- src/pages/Applications.jsx
+++ src/pages/Applications.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import AppCard from '../components/AppCard'
 import classes from '../styles/Applications.module.css'
+import { pageActions } from '../store/page-slice'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { motion } from 'framer-motion'
+import { useDispatch } from 'react-redux'
 
 const Applications = () => {
   const [apps, setApps] = useState([])
+  const dispatch = useDispatch()
   let number = 1
   useEffect(() => {
     fetch(
@@ -21,6 +25,13 @@ const Applications = () => {
   return (
     <motion.div animate={{ x: [-2000, 0] }}>
       <div className={classes.container}>
+        <div className={classes['back-button']}>
+          <AiOutlineArrowLeft
+            onClick={() => {
+              dispatch(pageActions.change(0))
+            }}
+          />
+        </div>
         <div className={classes['card-container']}>
           <h1>Submitted Applications</h1>
           {apps &&
